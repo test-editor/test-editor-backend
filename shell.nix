1dd01930feb4c8f0ca2267ea4f465040909967b9
Gunther Bachmann <gunther.bachmann@itemis.de>
2018-10-16 16:40:20 +0200

    jgit update, utf-8 setting in dev env

--- shell.nix
+++ shell.nix
@@ -11,6 +11,7 @@ stdenv.mkDerivation {
         alias g="./gradlew"
         alias g.="../gradlew"
         export GRADLE_OPTS="$GRADLE_OPTS -Dorg.gradle.daemon=false -Dfile.encoding=utf-8"
+        export _JAVA_OPTIONS="$_JAVA_OPTIONS -Dsun.jnu.encoding=UTF-8 -Dfile.encoding=UTF-8"
         # in case of any local java installations
         export JAVA_HOME=$(readlink $(dirname $(which java)))/..
     '';
